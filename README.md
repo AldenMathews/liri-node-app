11fd4ad771612a21d2dc00f4a54ad6bcbf24f60b
AldenM <bleedintrueblue33@yahoo.com>
2019-05-22 00:32:56 -0700

    Added code and psuedo code in readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # liri-node-app
+
+Write out code that will return band information such as future events (location and time).
+
+Write out code that will return movie information. Have it return title, debut time, IMDB rating, Rotten Tomatoe rating, plot and actors in movie.
--- liri.js
+++ liri.js
@@ -2,5 +2,21 @@ require("dotenv").config();
 import { spotify } from "./keys";
 
 var keys = require("./keys");
-var spotify = require("");
+var spotifyapi = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 
+spotify.search({ type: 'track', query: 'Hot For Teacher' }, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data);
+});
+
+spotify.search({ type: 'artist', query: 'Van Halen' }, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data);
+});
